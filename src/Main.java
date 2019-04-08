e526a6b739b7f4ce30c9e9eff8f61f5345eae2b7
Bly3t <erykcesarek256@gmail.com>
2019-04-08 22:11:03 +0200

    Just Main to get an access to methods

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.printf("Give a_length: ");
+        System.out.print("Give a_length: ");
         int x = reader.nextInt();
-        System.out.printf("Give r_bound: ");
+        System.out.print("Give r_bound: ");
         int y = reader.nextInt();
         Methods object = new Methods();
         object.Randomize(x, y);
