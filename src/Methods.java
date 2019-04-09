7ffa23a032fa06838dacdba03f5d0c610f2a400b
Bly3t <erykcesarek256@gmail.com>
2019-04-09 23:18:59 +0200

    Added souts

--- src/Methods.java
+++ src/Methods.java
@@ -1,21 +1,27 @@
 import java.util.Random;
+import java.util.Scanner;
 
- class Methods {
+class Methods {
+   private Scanner reader = new Scanner(System.in);
 
-         Methods()
-        {}
 
-     void Palindrome(String word) {
+     void Palindrome() {
+         System.out.println("Give a word: ");
+         String word = reader.nextLine();
         StringBuilder builder = new StringBuilder(word);
         System.out.print(word.equals(builder.reverse().toString()) ? "true" : "false");
     }
 
-    void Randomize(int a_length, int r_bound) {
-        int[] array = new int[a_length];
+    void Randomize() {
+        System.out.print("Give a_length: ");
+        int x_length = reader.nextInt();
+        System.out.print("Give r_bound: ");
+        int y_bound = reader.nextInt();
+        int[] array = new int[x_length];
         Random number = new Random();
 
         for(int y = 0; y < array.length; ++y) {
-            array[y] = number.nextInt(r_bound);
+            array[y] = number.nextInt(y_bound);
         }
 
         for(int x : array)
